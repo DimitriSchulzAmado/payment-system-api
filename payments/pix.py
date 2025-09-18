6b163a0b163b459cf92833cccacf0de51eedf0fc
Dimitri <dimitri.s.amado@gmail.com>
2025-09-18 09:17:51 -0300

    feat: Add Pix class for payment creation and QR code generation

---
+++ payments/pix.py
@@ -0,0 +1,22 @@
+import uuid
+import qrcode
+
+class Pix:
+    def __init__(self):
+        pass
+
+    def create_payment(self):
+        # Cria o pagamento na instituição financeira
+        bank_payment_id = uuid.uuid4() # Vem de uma instituição financeira
+        
+        # código copia e cola
+        hash_payment = f"hash_payment_{bank_payment_id}" # Vem de uma instituição financeira
+        
+        # qr code
+        img = qrcode.make(hash_payment)
+        img.save(f'static/img/qr_code_payment_{bank_payment_id}.png')
+        
+        return {
+            "payment_bank_id": bank_payment_id,
+            "qr_code_path": f'qr_code_payment_{bank_payment_id}'
+        }
